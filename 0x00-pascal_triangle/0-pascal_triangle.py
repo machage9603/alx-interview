7021a66368be75b8f03712cd98264149d11c80a0
machage9603 <mikemachage@gmail.com>
2024-10-13 17:15:02 +0300

    make files executable

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-Pascal Triangle
+This file focuses on Pascals's Triangle algorithm
 """
 
+
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal’s triangle of n."""
+    """
+    Returns a list of ists of integers representing the Pascal's triangle of n
+    """
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Start with the first row as [1]
+    triangle = [[1]]
 
     for i in range(1, n):
-        row = [1]  # First element of every row is 1
+        row = [1]
         for j in range(1, i):
-            # The next element is the sum of two elements directly above it
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)  # Last element of every row is 1
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
         triangle.append(row)
-
     return triangle
