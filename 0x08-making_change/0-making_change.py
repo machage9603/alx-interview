be9215ad95cfb5784726bce883d47e07f5b10927
machage9603 <mikemachage@gmail.com>
2024-12-03 20:20:43 +0300

    add task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module handles greedy algorithm to handle making change task"""
+
+
+def makeChange(coins, total):
+    """ Determines the fewest number of coins needed to a given amount total
+    Args:
+        - coins: a list of the values of coins in your possession
+        - total: a given amount
+    Return: fewest number of coins needed to meet total"""
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
